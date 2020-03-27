b9df7334b51ed539abcd27a0b4b068d99f034d65
steven <steven.attevelt@rorix.nl>
2020-03-27 16:12:05 +0100

    And yet another pending status found.

--- src/Message/TransactionResponse.php
+++ src/Message/TransactionResponse.php
@@ -7,13 +7,17 @@ use Omnipay\Common\Message\AbstractResponse;
 /**
  * Class TransactionResponse
  * @package Omnipay\Buckaroo\Message
+ * @see https://support.buckaroo.nl/categorie%C3%ABn/transacties/status
+ * @see https://support.buckaroo.nl/categorie%C3%ABn/transacties/refund
  */
 class TransactionResponse extends AbstractResponse
 {
     private const SUCCESS = 190;
-    private const PENDING_INPUT = 790;
-    private const PENDING = 791;
-    private const PENDING_APPROVAL = 794;
+    private const PENDING_INPUT = 790; // Awaiting input
+    private const PENDING_PROCESSING = 791; // Processing is taking place
+    private const PENDING_CUSTOMER_ACTION = 792; // Requires user action
+    private const PENDING_ON_HOLD = 793; // Most likely not enough credit for a refund
+    private const PENDING_APPROVAL = 794; // Awaiting manual approval
 
     /**
      * @inheritDoc
@@ -29,10 +33,11 @@ class TransactionResponse extends AbstractResponse
      */
     public function isPending(): bool
     {
-        // Pending Processing (791): The transaction is being processed.
         $pendingStatuses = [
             self::PENDING_INPUT,
-            self::PENDING,
+            self::PENDING_PROCESSING,
+            self::PENDING_CUSTOMER_ACTION,
+            self::PENDING_ON_HOLD,
             self::PENDING_APPROVAL
         ];
         return in_array($this->getCode(), $pendingStatuses);
