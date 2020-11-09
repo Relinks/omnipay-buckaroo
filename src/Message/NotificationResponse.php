dfd3ff9d99bba44c5811eb6ffb74aeffd4ee1040
nkop <niels@nielskop.nl>
2020-11-09 11:24:30 +0100

    add isRejected to notification repsonse

--- src/Message/NotificationResponse.php
+++ src/Message/NotificationResponse.php
@@ -53,6 +53,14 @@ class NotificationResponse implements NotificationInterface
     }
 
     /**
+     * @return bool
+     */
+    public function isRejected(): bool
+    {
+        return $this->data['brq_statuscode'] == 690;
+    }
+
+    /**
      * Response Message
      *
      * @return string A response message from the payment gateway
